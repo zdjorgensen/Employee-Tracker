55d154944475e004e74ec2a263c8d21931d28d90
Zach <z.d.jorgensen@gmail.com>
2022-09-07 19:57:57 -0600

    prompts and tables created

--- db/schema.sql
+++ db/schema.sql
@@ -5,29 +5,27 @@ USE cms_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
+    dept_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department)
+    dept_id INT,
+    FOREIGN KEY (dept_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role)
+    FOREIGN KEY (role_id)
     REFERENCES  role(id)
     ON DELETE SET NULL,
-    manager_id INT,
-    FOREIGN KEY(employee),
-    REFERENCES employee(id),
+    manager_id INT REFERENCES employee(id),
     ON DELETE SET NULL
-)
+);
---
+++ index.js
@@ -0,0 +1,89 @@
+const inquirer = require('inquirer');
+const { default: prompt } = require('inquirer/lib/ui/prompt');
+const mysql = require('mysql2');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Edisoncats1!',
+        database: 'cms_db'
+    },
+    console.log('working')
+);
+
+function menu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'userChoice',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department,', 'Quit']
+            },
+        ]).then((res) => {
+            switch (res.userChoice) {
+                case 'View All Employees':
+                    viewEmp();
+                    break;
+                case 'Add Employee':
+                    addEmp();
+                    break;
+                case 'Update Employee Role':
+                    updateEmp();
+                    break;
+                case 'View All Roles':
+                    viewRole();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    viewDept();
+                    break;
+                case 'Add Department':
+                    addDept();
+                case 'Quit':
+                    process.exit();
+            }
+        });
+}
+async function viewEmp() {
+    console.log('working');
+    const employee = await mysql.db.promise('SELECT * FROM employee')
+    db.query('SELECT * FROM employee')
+    menu();
+
+}
+async function addEmp() {
+    console.log('working');
+    menu();
+}
+async function updateEmp() {
+    console.log('working');
+    menu();
+}
+async function viewRole() {
+    console.log('working');
+    menu();
+}
+async function addRole() {
+    console.log('working');
+    menu();
+}
+async function viewDept() {
+    console.log('working');
+    menu();
+}
+async function addDept() {
+    console.log('working');
+    menu();
+}
+
+menu();
