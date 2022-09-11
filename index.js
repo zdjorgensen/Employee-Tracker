1b631271ac06f6c20a8042751fdbb28e22fcf04d
Zach <z.d.jorgensen@gmail.com>
2022-09-11 02:15:06 -0600

    add department function update

--- index.js
+++ index.js
@@ -15,82 +15,102 @@ const db = mysql.createConnection(
         password: 'password',
         database: 'cms_db'
     },
-    console.log('working')
 );
 
 function menu() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'What would you like to do?',
-                name: 'userChoice',
-                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department,', 'Quit']
-            },
-        ]).then((res) => {
-            switch (res.userChoice) {
-                case 'View All Employees':
-                    viewEmp();
-                    break;
-                case 'Add Employee':
-                    addEmp();
-                    break;
-                case 'Update Employee Role':
-                    updateEmp();
-                    break;
-                case 'View All Roles':
-                    viewRole();
-                    break;
-                case 'Add Role':
-                    addRole();
-                    break;
-                case 'View All Departments':
-                    viewDept();
-                    break;
-                case 'Add Department':
-                    addDept();
-                case 'Quit':
-                    process.exit();
-            }
-        });
-}
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'userChoice',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        },
+    ]).then((res) => {
+        switch (res.userChoice) {
+            case 'View All Employees':
+                viewEmp();
+                break;
+            case 'Add Employee':
+                addEmp();
+                break;
+            case 'Update Employee Role':
+                updateEmp();
+                break;
+            case 'View All Roles':
+                viewRole();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewDept();
+                break;
+            case 'Add Department':
+                addDept();
+                break;
+            case 'Quit':
+                process.exit();
+        }
+    });
+};
+
 async function viewEmp() {
     console.log('working');
-    // const employee = await db.promise('SELECT * FROM employee')
     db.query('SELECT * FROM employee', function (err, results) {
         console.log(results);
     })
-    menu();
+    await menu();
 
-}
+};
+
 async function addEmp() {
+
     console.log('working');
-    menu();
-}
+    await menu();
+};
+
 async function updateEmp() {
     console.log('working');
-    menu();
-}
+    await menu();
+};
+
 async function viewRole() {
     db.query('SELECT * FROM emp_role', function (err, results) {
         console.log(results);
     })
-    menu();
-}
+    await menu();
+};
+
 async function addRole() {
     console.log('working');
-    menu();
-}
+    await menu();
+};
+
 async function viewDept() {
     console.log('working');
     db.query('SELECT * FROM department', function (err, results) {
         console.log(results);
     })
-    menu();
-}
+    await menu();
+};
+
 async function addDept() {
-    console.log('working');
-    menu();
-}
+    const test = await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'dept_name',
+        },
+    ]).then((res) => {
+        console.log(res.dept_name);
+        db.query(`INSERT INTO department (dept_name) VALUES (?)`, res.dept_name, (err => {
+            if(err) {
+                console.log(err)
+            }
+            console.log("inserted");
+        }));
+    })
+    await menu();
+};
 
 menu();
